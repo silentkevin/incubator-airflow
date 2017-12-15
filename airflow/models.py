d226b7c560533cdde632ab6e27cbd6e4d33cc3b4
Kevin Brown <silentkevin@gmail.com>
2017-12-15 11:10:27 -0500

    applying patch from Stibbons fork eb8938bcda3a51de6156442139d1be8167f3d74a to allow base url change and adding a bit of fixup myself to resolve conflicts

--- airflow/models.py
+++ airflow/models.py
@@ -988,6 +988,7 @@ class TaskInstance(Base, LoggingMixin):
         iso = self.execution_date.isoformat()
         BASE_URL = configuration.get('webserver', 'BASE_URL')
         return BASE_URL + (
+            configuration.get_url_prefix() +
             "/admin/airflow/log"
             "?dag_id={self.dag_id}"
             "&task_id={self.task_id}"
@@ -999,6 +1000,7 @@ class TaskInstance(Base, LoggingMixin):
         iso = self.execution_date.isoformat()
         BASE_URL = configuration.get('webserver', 'BASE_URL')
         return BASE_URL + (
+            configuration.get_url_prefix() +
             "/admin/airflow/action"
             "?action=success"
             "&task_id={self.task_id}"
